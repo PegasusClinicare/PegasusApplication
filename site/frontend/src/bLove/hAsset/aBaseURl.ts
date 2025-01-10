7dedc1042ed81373663a6f5e5f3234de75dd3c39
Harshal <harshal.madgulkar@sdaemon.co.in>
2025-01-10 19:30:21 +0530

    Base URLs for images changed in frontend

--- site/frontend/src/bLove/hAsset/aBaseURl.ts
+++ site/frontend/src/bLove/hAsset/aBaseURl.ts
@@ -1,5 +1,6 @@
-const baseURL = import.meta.env.VITE_ENVIRONMENT === "Production" ? 
-  "https://pegasus-backend-f4bi.onrender.com/api/v1" : 
+const baseURL = import.meta.env.VITE_ENVIRONMENT === "Production" ? "hsagdgsadhsagdgsadsad/api/v1" : 
+  import.meta.env.VITE_ENVIRONMENT === "Practice" ? "https://pegasus-backend-kc6j.onrender.com/api/v1" : 
+  import.meta.env.VITE_ENVIRONMENT === "Development" ? "http://localhost:8080/api/v1" : 
   "http://localhost:8080/api/v1"
 
 export default baseURL;
