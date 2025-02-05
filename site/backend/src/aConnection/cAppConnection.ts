b16cbcb02d4c16eec22f074238cc81b855a3a03e
Harshal <harshal.madgulkar@sdaemon.co.in>
2025-02-05 10:44:38 +0530

    CORS policy changged

--- site/backend/src/aConnection/cAppConnection.ts
+++ site/backend/src/aConnection/cAppConnection.ts
@@ -1,52 +1,48 @@
-import express from 'express'
+import express from "express";
 
-import morganMiddleware from 'morgan'
-import corsMiddleware from 'cors'
-import bodyParserMiddleware from 'body-parser'
-import cookieParserMiddleware from 'cookie-parser'
-import compressionMiddleware from 'compression'
+import morganMiddleware from "morgan";
+import corsMiddleware from "cors";
+import bodyParserMiddleware from "body-parser";
+import cookieParserMiddleware from "cookie-parser";
+import compressionMiddleware from "compression";
 
-import errorMiddleware from '../bLove/bMiddleware/aErrorMiddleware'
+import errorMiddleware from "../bLove/bMiddleware/aErrorMiddleware";
 
-// import { baseManyToOneRouter } from '../bLove/aMCR/cRoute/aSetting/aBaseManyToOneRoute'
-// import { baseManyToManyRouter } from '../bLove/aMCR/cRoute/aSetting/bBaseManyToManyRoute'
-// import { baseRouter } from '../bLove/aMCR/cRoute/aSetting/cBaseRoute'
-// import { baseOneToOneRouter } from '../bLove/aMCR/cRoute/aSetting/dBaseOneToOneRoute'
-// import { baseOneToManyRouter } from '../bLove/aMCR/cRoute/aSetting/eBaseOneToManyRoute'
+import { userRouter } from "../bLove/aMCR/cRoute/bUserAdministration/aUserRoute";
+import { roleRouter } from "../bLove/aMCR/cRoute/bUserAdministration/bRoleRoute";
+import { menuRouter } from "../bLove/aMCR/cRoute/bUserAdministration/cMenuRoute";
 
-import { userRouter } from '../bLove/aMCR/cRoute/bUserAdministration/aUserRoute'
-import { roleRouter } from '../bLove/aMCR/cRoute/bUserAdministration/bRoleRoute'
-import { menuRouter } from '../bLove/aMCR/cRoute/bUserAdministration/cMenuRoute'
+import { organizationRouter } from "../bLove/aMCR/cRoute/dPegasusMain/aOrganizationRoute";
+import { licenseRouter } from "../bLove/aMCR/cRoute/dPegasusMain/bLicenseRoute";
+import { serviceRouter } from "../bLove/aMCR/cRoute/dPegasusMain/cServiceRoute";
+import { documentRouter } from "../bLove/aMCR/cRoute/dPegasusMain/dDocumentRoute";
+import { inspectionRouter } from "../bLove/aMCR/cRoute/dPegasusMain/eInspectionRoute";
+import { enrolledServiceRouter } from "../bLove/aMCR/cRoute/dPegasusMain/fEnrolledServiceRoute";
 
-import { organizationRouter } from '../bLove/aMCR/cRoute/dPegasusMain/aOrganizationRoute'
-import { licenseRouter } from '../bLove/aMCR/cRoute/dPegasusMain/bLicenseRoute'
-import { serviceRouter } from '../bLove/aMCR/cRoute/dPegasusMain/cServiceRoute'
-import { documentRouter } from '../bLove/aMCR/cRoute/dPegasusMain/dDocumentRoute'
-import { inspectionRouter } from '../bLove/aMCR/cRoute/dPegasusMain/eInspectionRoute'
-import { enrolledServiceRouter } from '../bLove/aMCR/cRoute/dPegasusMain/fEnrolledServiceRoute'
-
-import { singleImageRouter } from '../bLove/aMCR/cRoute/zFreestyleSample/aSingleImageRoute'
-
+import { singleImageRouter } from "../bLove/aMCR/cRoute/zFreestyleSample/aSingleImageRoute";
 
 const appConnection = express();
 
 // Third Party Middleware
 appConnection.use(morganMiddleware("dev"));
-appConnection.use(corsMiddleware({
-  origin: process.env.ENVIRONMENT === "Production" ?
-    [
-      "http://13.60.80.5:8080",
-    ] : process.env.ENVIRONMENT === "Practice" ?
-      [
-        "https://pegasus-practice.netlify.app",
-        "https://app.pegasusclinicare.com"
-      ] : process.env.ENVIRONMENT === "Development" ?
-        [
-          "http://localhost:5173",
-          "http://localhost:5174"
-        ] : "http://localhost:5173",
-  credentials: true
-}));
+appConnection.use(
+  corsMiddleware({
+    origin:
+      process.env.ENVIRONMENT === "Production"
+        ? ["http://13.60.80.5:8080"]
+        : process.env.ENVIRONMENT === "Practice"
+        ? [
+            "https://pegasus-practice.netlify.app",
+            "https://app.pegasusclinicare.com",
+          ]
+        : process.env.ENVIRONMENT === "Development"
+        ? ["http://localhost:5173", "http://localhost:5174"]
+        : "http://localhost:5173",
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 appConnection.use(bodyParserMiddleware.urlencoded({ extended: true }));
 appConnection.use(bodyParserMiddleware.json());
 appConnection.use(cookieParserMiddleware());
@@ -64,7 +60,7 @@ appConnection.use("/api/v1/document/", documentRouter);
 appConnection.use("/api/v1/inspection/", inspectionRouter);
 appConnection.use("/api/v1/enrolled-service/", enrolledServiceRouter);
 
-appConnection.use('/api/v1/single-image/', singleImageRouter);
+appConnection.use("/api/v1/single-image/", singleImageRouter);
 
 // Error Middleware
 appConnection.use(errorMiddleware);
