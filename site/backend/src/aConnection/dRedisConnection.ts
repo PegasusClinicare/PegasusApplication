cd5f7ebd1cb2d90d04f2e0c3f676e54662bec034
PegasusClinicare <pegasusintime@gmail.com>
2025-09-06 11:49:24 +0530

    Update dRedisConnection.ts

--- site/backend/src/aConnection/dRedisConnection.ts
+++ site/backend/src/aConnection/dRedisConnection.ts
@@ -3,15 +3,15 @@ import Redis from 'ioredis'
 
 const redisClient = new Redis(process.env.ENVIRONMENT === "Production" ? (
   {
-    host: 'redis-19708.c301.ap-south-1-1.ec2.redns.redis-cloud.com',
-    port: 19708,
-    password: '1vlBGdXUIVXFEVkegOnXtcWFPMDAEnwI'
+    host: 'redis-10983.c301.ap-south-1-1.ec2.redns.redis-cloud.com',
+    port: 10983,
+    password: 'kypo8F2ywgAPkUUfpb7TLeYMM05XK9i7'
   }
 ) : (
   {
-    host: 'redis-19708.c301.ap-south-1-1.ec2.redns.redis-cloud.com', // not working yet. create on pegasusclinicare 
-    port: 19708,
-    password: '1vlBGdXUIVXFEVkegOnXtcWFPMDAEnwI'
+    host: 'redis-10983.c301.ap-south-1-1.ec2.redns.redis-cloud.com', // not working yet. create on pegasusclinicare 
+    port: 10983,
+    password: 'kypo8F2ywgAPkUUfpb7TLeYMM05XK9i7'
   }
 ))
 
